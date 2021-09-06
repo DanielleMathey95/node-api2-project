43458ec995d6e8feca27bbeb349a86f0c81048b5
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-05 21:48:50 -0700

    POST request now complete and functioning properly

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 //ENDPOINTS HERE
 
-//[GET] /api/posts returns an array of all post objects contained in the database
+//[GET] /api/posts (R of CRUD) returns an array of all post objects contained in the database
 
 router.get("/", (req, res) => {
   Posts.find()
@@ -22,18 +22,41 @@ router.get("/", (req, res) => {
     });
 });
 
-//[GET] api/posts/:id get posts with specific id
+//[GET] api/posts/:id (R of CRUD) get posts with specific id
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  Posts.findById(id).then((post) =>
-    !post
-      ? res.status(404).json("The post with the specified ID does not exist")
-      : res.json(post)
-  )
-  .catch((error) => {
-    res.status(500).json({message: "The post information could not be retrieved"})
-  })
+  Posts.findById(id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    }
+  });
+});
+
+//[POST] api/posts (C of CRUD) Creates a new post
+router.post("/", (req, res) => {
+  const postInfo = req.body;
+
+  if (!postInfo.title || !postInfo.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Posts.insert(postInfo)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
 });
 
 module.exports = router;
